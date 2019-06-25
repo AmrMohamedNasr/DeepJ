74a4697d166273699d9ac34cf004a91714c1392d
Amr Nasr <re13amr@gmail.com>
2019-06-25 21:51:03 +0000

    fix permissions in commands

--- setup_enviroment.sh
+++ setup_enviroment.sh
@@ -6,13 +6,13 @@ python3 -m pip install gputil
 python3 -m pip install psutil
 python3 -m pip install humanize
 python3 -m pip install PyDrive
-
+python3 -m pip install joblib
 unzip data.zip
 
 git clone https://github.com/louisabraham/python3-midi.git
-apt-get install -y swig
+sudo apt-get install -y swig
 cd python3-midi
-python3 setup.py install
+sudo python3 setup.py install
 cd ..
 
 python3 -m pip install keras==2.0.8
